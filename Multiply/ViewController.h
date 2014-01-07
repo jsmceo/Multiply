aff372197a64ac289b546526028dc59331eb2272
jsmceo <john.s.malloy@gmail.com>
2014-01-07 15:53:43 -0600

    Initial commit

---
+++ Multiply/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Multiply
+//
+//  Created by John Malloy on 1/7/14.
+//  Copyright (c) 2014 Big Red INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Multiply/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Multiply
+//
+//  Created by John Malloy on 1/7/14.
+//  Copyright (c) 2014 Big Red INC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
